b05cb469aec774f49d7e6debe0dd499dabc82b42
Nikolay <promath.bul@gmail.com>
2025-02-24 19:52:03 +0300

    Создан простой тест c помощью модуля unittest

---
+++ test_abs_project.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestAbs(unittest.TestCase):
+    def test_abs_1(self):
+        self.assertEqual(abs(-42), 42, "Ожидается абсолютное значение числа")
+
+    def test_abs_1(self):
+        self.assertEqual(abs(-42), -42, "Ожидается абсолютное значение числа")
+
+if __name__ == '__main__':
+    unittest.main()
