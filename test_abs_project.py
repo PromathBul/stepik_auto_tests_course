bd85a8e8ed14f27a2530441aa140ffd13c8ab461
Nikolay <promath.bul@gmail.com>
2025-02-25 14:44:20 +0300

    Исправлена ошибка в названии второго метода

--- test_abs_project.py
+++ test_abs_project.py
@@ -4,7 +4,7 @@ class TestAbs(unittest.TestCase):
     def test_abs_1(self):
         self.assertEqual(abs(-42), 42, "Ожидается абсолютное значение числа")
 
-    def test_abs_1(self):
+    def test_abs_2(self):
         self.assertEqual(abs(-42), -42, "Ожидается абсолютное значение числа")
 
 if __name__ == '__main__':
